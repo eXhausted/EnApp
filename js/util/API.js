99d9432cdfc6e8d23fa27c8edc8d31fa12e0d675
RomanGorodyanenko <necto68necto68@gmail.com>
2017-06-27 18:17:56 +0300

    try login with cookie. failed :(

--- js/util/API.js
+++ js/util/API.js
@@ -8,9 +8,14 @@ class API {
     static async getGameModal(requestData = {}) {
         let response = await axios({
             url: `http://necto68.url.ph/game/?g=${ALIAS_NECTO68_MIRROR}&json=1`,
+            // url: 'http://demo.en.cx/gameengines/encounter/play/26273?json=1',
             method: 'post',
             timeout: 12000,
             data: qs.stringify(requestData),
+            withCredentials: true,
+            /* headers: {
+                Cookie: 'GUID=6773fd25%2Dabbf%2D4a52%2D918f%2D5fd7d3e4d293; atoken=uid%3d159224%26iss%3d0%26iscd%3d1%26tkn%3d72c671ff978c7b896bc82b04ce12f85dc1fbf3c1; stoken=gihljd4utoa45yn5ycbnjrre;',
+            }, */
         });
 
         response = response.data;
